828ee5437211b279f6d032865dbccdf61c458dfd
Mazsak <mazsak97@gmail.com>
2020-10-28 21:59:21 +0100

    change models

--- app/workflows/models.py
+++ app/workflows/models.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from django.conf import settings
 from django.db import models
 
-
 STATUS_VALUES = (
     ('ready', 'Ready to run'),
     ('running', 'Running right now'),
@@ -16,35 +15,39 @@ STATUS_VALUES = (
 
 # Create your models here.
 class Subtask(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE)
     name = models.CharField(max_length=60)
     notes = models.CharField(max_length=1000)
     script_path = models.FilePathField()
     created_on = models.DateTimeField(default=datetime.now)
     updated_on = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=32, choices=STATUS_VALUES,
-        default='ready')
+                              default='ready')
+
 
 class Task(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE)
     name = models.CharField(max_length=60)
     notes = models.CharField(max_length=1000)
     created_on = models.DateTimeField(default=datetime.now)
     updated_on = models.DateTimeField(auto_now=True)
     subtasks = models.ManyToManyField(Subtask)
     status = models.CharField(max_length=32, choices=STATUS_VALUES,
-        default='ready')
+                              default='ready')
+
 
 class Workflow(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE, related_name='author')
+                               on_delete=models.CASCADE, related_name='author')
     name = models.CharField(max_length=60)
     notes = models.CharField(max_length=1000)
     created_on = models.DateTimeField(default=datetime.now)
     updated_on = models.DateTimeField(auto_now=True)
     users = models.ManyToManyField(settings.AUTH_USER_MODEL,
-        related_name='users')
+                                   related_name='users')
+    tasks = models.ManyToManyField(Task)
     status = models.CharField(max_length=32, choices=STATUS_VALUES,
-        default='ready')
+                              default='ready')
+
+
+class Dictionary(models.Model):
+    argument = models.CharField(max_length=100)
+    eval_str = models.CharField(max_length=100)
