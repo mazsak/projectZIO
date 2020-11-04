40ca9599d1c7c402cbbeaf43916706cccdad2742
Mazsak <mazsak97@gmail.com>
2020-11-04 14:40:02 +0100

    create view register

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,5 +21,6 @@ from workflows import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
     path('admin/', admin.site.urls),
 ]
--- app/workflows/views.py
+++ app/workflows/views.py
@@ -6,5 +6,10 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'pages/index.html')
 
+
 def login(request):
     return render(request, 'pages/login.html')
+
+
+def register(request):
+    return render(request, 'pages/register.html')
