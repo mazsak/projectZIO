1e443d77a582b4610137bafd94c2491d5a93a213
mazsak <mazsak97@gmial.com>
2020-12-03 13:07:16 +0100

    nextflows

--- app/app/tasks.py
+++ app/app/tasks.py
@@ -1,14 +1,20 @@
 from subprocess import Popen, PIPE, STDOUT
 
 from celery import shared_task, chain, group
-
 from workflows.models import Task, Subtask
 
 
 @shared_task(bind=True)
-def launch_subtask(self, data):
+def fake_task(self, *args, **kwargs):
+    print("FAKE TASK")
+
+
+@shared_task(bind=True)
+def launch_subtask(self, *args, **kwargs):
+    data = kwargs.get('data', None)
+    print(f'\n\n\n data in launch subtask: {data} \n\n\n')
     res = []
-    subtask = list(Subtask.objects.filter(id=data['subtask_id']))[0]
+    subtask = list(Subtask.objects.filter(id=data['id']))[0]
     subtask.status = 'STARTED'
     subtask.save()
     process = Popen(
@@ -21,41 +27,12 @@ def launch_subtask(self, data):
     )
 
     for line in iter(process.stdout.readline, b''):
-        line = line.decode('utf-8')
-        res.append(line)
-        print(line)
+        with open(f"log_{data['workflow_id']}_{data['task_id']}_{subtask.id}.txt", "a") as f:
+            line = line.decode('utf-8')
+            res.append(line)
+            f.write(line)
+            print(line)
     process.wait()
     subtask.status = 'SUCCESS'
     subtask.save()
-    return res
-
-
-@shared_task(bind=True)
-def launch_task(self, data):
-    responses = []
-    res = []
-
-    task = list(Task.objects.filter(id=data['task_id']))[0]
-    task.status = 'STARTED'
-    task.save()
-    subtasks = []
-    subtasks_with_prev = []
-    for subtask in task.subtasks.all():
-        if not subtask.skip:
-            if subtask.run_with_previous:
-                subtasks_with_prev.append(subtask)
-            else:
-                if subtasks_with_prev:
-                    subtasks.append(subtasks_with_prev)
-                subtasks_with_prev = [subtask]
-    subtasks.append(subtasks_with_prev)
-
-    for subtasks_group in subtasks:
-        for subtask in subtasks_group:
-            subtask.status = 'PENDING'
-            subtask.save()
-    ch = chain(*[group(*[launch_task.s({'subtask_id': subtask.id}) for subtask in subtasks_group]) for subtasks_group in subtasks]).apply_async()
-
-    task.status = 'SUCCESS'
-    task.save()
     return res
